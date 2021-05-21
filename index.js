350fe527d0cb3443efb99a27ca21552629020dae
ofir-danan <77383793+ofir-danan@users.noreply.github.com>
2021-05-21 10:57:50 +0300

    files added

---
+++ index.js
@@ -0,0 +1,171 @@
+require("dotenv").config();
+const Web3 = require("web3");
+const fetch = require("node-fetch");
+const web3 = new Web3(
+  new Web3("https://mainnet.infura.io/v3/5fc4a237507c477cab9666203f3847a4")
+);
+const provider = new Web3.providers.HttpProvider(
+  "https://mainnet.infura.io/v3/5fc4a237507c477cab9666203f3847a4"
+);
+const Contract = require("web3-eth-contract");
+Contract.setProvider(provider);
+
+const apikey = process.env.API_KEY; // to access etherscan API
+const contract = process.env.CONTRACT; // contract address
+const uniswapRouterAccount = process.env.UNISWAP_ADDRESS; // the address of the pool
+
+const account = "0xFcd300AaFE1fDB3166cd1A3B46463144fc2D46ad";
+
+let uniswapLPContract,
+  pricePerUnit,
+  currentValueInUSD,
+  ABI,
+  ethUsd,
+  providedUSD,
+  providedETH;
+
+// Gets the ABI json of the contract
+const uniswapLPContractABI = async () => {
+  ABI = await fetch(
+    `https://api.etherscan.io/api?module=contract&action=getabi&address=${contract}&apikey=${apikey}`
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      if (body.status === "1") {
+        const res = JSON.parse(body.result);
+
+        return res;
+      }
+    });
+
+  uniswapLPContract = new Contract(ABI, contract);
+};
+
+const getDecimal = async (contract, address) => {
+  let contractDecimal = await contract.methods
+    .decimals()
+    .call({ from: address });
+  return Number(`1e${contractDecimal}`);
+};
+
+async function getDataAboutThePool() {
+  await uniswapLPContractABI();
+  if (uniswapLPContract) {
+    let totalSupply = await uniswapLPContract.methods
+      .totalSupply()
+      .call({ from: account });
+
+    let contractDecimal = await getDecimal(uniswapLPContract, account);
+
+    totalSupply = totalSupply / contractDecimal;
+
+    // the uniswap token balance of the account
+    const lpBalance =
+      Number(
+        await uniswapLPContract.methods
+          .balanceOf(account)
+          .call({ from: account })
+      ) / contractDecimal;
+
+    // the amount of each token in the pool
+    let reserves = await uniswapLPContract.methods
+      .getReserves()
+      .call({ from: account });
+
+    const token0 = await uniswapLPContract.methods[`token0`]().call({
+      from: account,
+    });
+    const token0Contract = new Contract(ABI, token0);
+    let token0Decimal = await getDecimal(token0Contract, account);
+
+    const token1 = await uniswapLPContract.methods[`token1`]().call({
+      from: account,
+    });
+    const token1Contract = new Contract(ABI, token1);
+    let token1Decimal = await getDecimal(token1Contract, account);
+
+    reserves = {
+      token0: reserves[0] / token0Decimal,
+      token1: reserves[1] / token1Decimal,
+    };
+
+    // the price of UNISWAP token
+    const token1PriceInUSD = reserves.token0 / reserves.token1;
+
+    pricePerUnit =
+      (reserves.token1 * token1PriceInUSD + reserves.token0) / totalSupply;
+
+    // how much his part in the pool worth
+    currentValueInUSD = lpBalance * pricePerUnit;
+  }
+}
+
+// All the API's used to fetch data about the tokens
+async function getTokenData() {
+  //Eth price in USD
+  ethUsd = await fetch(
+    `https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${apikey}`
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      return body.result.ethusd;
+    });
+
+  //Finding how much USD was sent to the pool
+  providedUSD = await fetch(
+    `https://api.etherscan.io/api?module=account&action=tokentx&address=${account}&startblock=0&endblock=999999999&sort=asc&apikey=${apikey}`
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      const { result } = body;
+      let transactions = result.slice();
+      let providedUSDTxn = transactions.find(
+        (transaction) => transaction.from === account.toLowerCase()
+      );
+      const providedUsd =
+        providedUSDTxn.value / Math.pow(10, providedUSDTxn.tokenDecimal);
+      return providedUsd;
+    });
+
+  //Finding how much eth was sent to the pool
+  providedETH = await fetch(
+    `https://api.etherscan.io/api?module=account&action=txlist&address=${account}&startblock=0&endblock=99999999&sort=asc&apikey=${apikey}`
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      let transactions = body.result;
+      transactions = transactions?.filter((transaction) => {
+        if (transaction.to === uniswapRouterAccount) {
+          return true;
+        }
+        return false;
+      });
+
+      let transactionValues = transactions?.map((transaction) => {
+        return web3.utils.fromWei(transaction.value, "ether");
+      });
+
+      transactionValues = transactionValues?.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      );
+
+      return transactionValues;
+    });
+}
+
+async function calculateTheImpermanentLoss() {
+  await getDataAboutThePool();
+  await getTokenData();
+
+  const couldBeTheValue = providedETH * ethUsd + providedUSD;
+  let impermanent_loss = (couldBeTheValue - currentValueInUSD).toFixed(4);
+  let impermanent_loss_percent = (couldBeTheValue / currentValueInUSD).toFixed(
+    4
+  );
+
+  console.log(
+    `The impermanent loss is:\x1b[32m ${impermanent_loss}\x1b[37m USD, and \x1b[32m${impermanent_loss_percent}%\x1b[37m`
+  );
+}
+
+calculateTheImpermanentLoss();
